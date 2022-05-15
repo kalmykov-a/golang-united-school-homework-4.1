8783c5ea6ae4291dbfb0fcc8ddf32b6c92d82dca
kalmykov-a <kalmykoff93@mail.ru>
2022-05-15 17:10:39 +0300

    Part 1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,13 @@
 package reverse_string
 
+import "unicode/utf8"
+
 func ReverseString(input string) (output string) {
 	// solution goes here
+	var res []rune
+	for i, r := range input {
+		res[utf8.RuneCountInString(input)-1-i] = r
+	}
+	output = string(res)
 	return output
 }
